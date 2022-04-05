2ab1c19bbe47573bcfe37c8b3d9a78149cb9f539
Sak32009 <Sak32009@users.noreply.github.com>
2022-04-05 20:49:22 +0200

    If steamRetriever returns an error with the achievements, stats or items the game is added anyway and no longer interrupts the console.

--- src/main/src/classes/steam-retriever.ts
+++ src/main/src/classes/steam-retriever.ts
@@ -8,6 +8,9 @@ import {
 import {
   join,
 } from 'node:path';
+import type {
+  AxiosError,
+} from 'axios';
 import axios from 'axios';
 import {
   webContents,
@@ -46,9 +49,31 @@ class SteamRetriever {
     this.gamePaths = gamePathsByAppId(inputs.appId);
   }
 
-  private console (content: string, space = false) {
-    log.info(content);
-    this.ipcEvent.send('console-add', content, space);
+  private console (content: AxiosError | Error | string, space = false) {
+    let nContent = content;
+    if (typeof nContent === 'string') {
+      log.info(nContent);
+    } else {
+      if (axios.isAxiosError(nContent)) {
+        const url = new URL(nContent.config.url!);
+        const urlSearchParameters = url.searchParams;
+        const urlQueryPrivacy = '_PRIVACY_';
+
+        if (urlSearchParameters.has('key')) {
+          urlSearchParameters.set('key', urlQueryPrivacy);
+        }
+
+        if (urlSearchParameters.has('digest')) {
+          urlSearchParameters.set('digest', urlQueryPrivacy);
+        }
+
+        nContent = `${url.href}; response: ${nContent.message}`;
+      }
+
+      log.error(nContent);
+    }
+
+    this.ipcEvent.send('console-add', typeof nContent === 'string' ? nContent : nContent.message, space);
   }
 
   private consoleHide (isOk = false) {
@@ -64,23 +89,18 @@ class SteamRetriever {
 
     const url = `https://store.steampowered.com/api/appdetails/?appids=${this.gameAppId}&filters=basic`;
     const response = await axios.get(url);
+    const data = response.data[this.gameAppId];
+    if (data.success === true) {
+      if (data.data.type === 'game') {
+        this.console('The appid is the "game" type, keep on...', true);
 
-    if (response.statusText === 'OK') {
-      const data = response.data[this.gameAppId];
-      if (data.success === true) {
-        if (data.data.type === 'game') {
-          this.console('The appid is the "game" type, keep on...', true);
-
-          // await ensureDir(this.paths.appIdDataPath);
-          await ensureDir(this.gamePaths.appIdAchievementsPath);
-        } else {
-          throw new Error('The appid is not a game.');
-        }
+        // await ensureDir(this.paths.appIdDataPath);
+        await ensureDir(this.gamePaths.appIdAchievementsPath);
       } else {
-        throw new Error('The appid was not found.');
+        throw new Error('The appid is not a game.');
       }
     } else {
-      throw new Error(`${url}; response: ${response.statusText}`);
+      throw new Error('The appid was not found.');
     }
   }
 
@@ -126,36 +146,31 @@ class SteamRetriever {
 
     const url = `https://store.steampowered.com/api/dlcforapp/?appid=${this.gameAppId}`;
     const response = await axios.get(url);
+    if (response.data.status === 1) {
+      this.gameData.name = response.data.name;
 
-    if (response.statusText === 'OK') {
-      if (response.data.status === 1) {
-        this.gameData.name = response.data.name;
+      this.console(`NAME: ${this.gameData.name}; DLCS: ${response.data.dlc.length}`, true);
 
-        this.console(`NAME: ${this.gameData.name}; DLCS: ${response.data.dlc.length}`, true);
-
-        if (response.data.dlc.length > 0) {
-          const resultDlcs = [];
-          for (const appDlcData of response.data.dlc) {
-            const appDlcAppId = appDlcData.id;
-            const appDlcMame = appDlcData.name;
-            this.console(`DLC AppID: ${appDlcAppId}; DLC Name: ${appDlcMame}`, true);
-            resultDlcs.push(`${appDlcAppId}=${appDlcMame}`);
-          }
-
-          await writeFile(this.gamePaths.appIdDlcsInfoPath, resultDlcs.join('\r\n')).then(() => {
-            this.console(
-              `${this.gamePaths.appIdDlcsInfoPath} was written successfully!`,
-              true,
-            );
-          });
-        } else {
-          this.console('The game does not contain dlcs.', true);
+      if (response.data.dlc.length > 0) {
+        const resultDlcs = [];
+        for (const appDlcData of response.data.dlc) {
+          const appDlcAppId = appDlcData.id;
+          const appDlcMame = appDlcData.name;
+          this.console(`DLC AppID: ${appDlcAppId}; DLC Name: ${appDlcMame}`, true);
+          resultDlcs.push(`${appDlcAppId}=${appDlcMame}`);
         }
+
+        await writeFile(this.gamePaths.appIdDlcsInfoPath, resultDlcs.join('\r\n')).then(() => {
+          this.console(
+            `${this.gamePaths.appIdDlcsInfoPath} was written successfully!`,
+            true,
+          );
+        });
       } else {
-        throw new Error('The appid was not found.');
+        this.console('The game does not contain dlcs.', true);
       }
     } else {
-      throw new Error(`${url}; response: ${response.statusText}`);
+      throw new Error('The appid was not found.');
     }
   }
 
@@ -174,82 +189,77 @@ class SteamRetriever {
   private async getAppAchievements () {
     this.console(`Trying to get achievements and stats on ${this.gameAppId}...`);
 
-    const url = `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?l=${this.accountLanguage}&key=${this.accountSteamWebApiKey}&appid=${this.gameAppId}`;
+    const url = `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=${this.accountSteamWebApiKey}&l=${this.accountLanguage}&appid=${this.gameAppId}`;
     const response = await axios.get(url);
+    const achievementsData = response.data.game.availableGameStats.achievements;
+    if (typeof achievementsData !== 'undefined' && achievementsData.length > 0) {
+      const resultAchievements = [];
+      for (const achievement of achievementsData) {
+        const iconUrl = achievement.icon;
+        const iconGrayUrl = achievement.icongray;
 
-    if (response.statusText === 'OK') {
-      const achievementsData = response.data.game.availableGameStats.achievements;
-      if (typeof achievementsData !== 'undefined' && achievementsData.length > 0) {
-        const resultAchievements = [];
-        for (const achievement of achievementsData) {
-          const iconUrl = achievement.icon;
-          const iconGrayUrl = achievement.icongray;
+        const iconName = `${achievement.name}.jpg`;
+        const iconGrayName = `${achievement.name}_locked.jpg`;
 
-          const iconName = `${achievement.name}.jpg`;
-          const iconGrayName = `${achievement.name}_locked.jpg`;
+        const iconNamePath = join(this.gamePaths.appIdAchievementsPath, iconName);
+        const iconGrayNamePath = join(this.gamePaths.appIdAchievementsPath, iconGrayName);
 
-          const iconNamePath = join(this.gamePaths.appIdAchievementsPath, iconName);
-          const iconGrayNamePath = join(this.gamePaths.appIdAchievementsPath, iconGrayName);
+        achievement.icon = `achievements/${iconName}`;
+        achievement.icongray = `achievements/${iconGrayName}`;
 
-          achievement.icon = `achievements/${iconName}`;
-          achievement.icongray = `achievements/${iconGrayName}`;
+        resultAchievements.push(achievement);
 
-          resultAchievements.push(achievement);
-
-          // eslint-disable-next-line no-negated-condition
-          if (!(await pathExists(iconNamePath))) {
-            await this.imageDownloader(iconUrl, iconNamePath).then(() => {
-              this.console(`${iconName} was downloaded successfully!`, true);
-            });
-          } else {
-            this.console(`${iconName} already exists, skip...`, true);
-          }
-
-          // eslint-disable-next-line no-negated-condition
-          if (!(await pathExists(iconGrayNamePath))) {
-            await this.imageDownloader(iconGrayUrl, iconGrayNamePath).then(() => {
-              this.console(`${iconGrayName} was downloaded successfully!`, true);
-            });
-          } else {
-            this.console(`${iconGrayName} already exists, skip...`, true);
-          }
+        // eslint-disable-next-line no-negated-condition
+        if (!(await pathExists(iconNamePath))) {
+          await this.imageDownloader(iconUrl, iconNamePath).then(() => {
+            this.console(`${iconName} was downloaded successfully!`, true);
+          });
+        } else {
+          this.console(`${iconName} already exists, skip...`, true);
         }
 
-        await writeJson(this.gamePaths.appIdAchievementsInfoPath, resultAchievements, {
-          spaces: 2,
-        }).then(() => {
-          this.console(
-            `${this.gamePaths.appIdAchievementsInfoPath} was written successfully!`,
-            true,
-          );
-        });
-      } else {
-        this.console('The game has no achievements.', true);
-      }
-
-      const statsData = response.data.game.availableGameStats.stats;
-      if (typeof statsData !== 'undefined' && statsData.length > 0) {
-        const resultStats = [];
-        // eslint-disable-next-line @typescript-eslint/naming-convention, canonical/id-match
-        for (const _stat of statsData) {
-          // eslint-disable-next-line @typescript-eslint/naming-convention, canonical/id-match
-          const name = _stat.name;
-          // NOTE: ..., float, avgrate but where can I find this data?
-          const typeValue = 'int';
-          // eslint-disable-next-line @typescript-eslint/naming-convention, canonical/id-match
-          const defaultValue = _stat.defaultvalue;
-
-          resultStats.push(`${name}=${typeValue}=${defaultValue}`);
+        // eslint-disable-next-line no-negated-condition
+        if (!(await pathExists(iconGrayNamePath))) {
+          await this.imageDownloader(iconGrayUrl, iconGrayNamePath).then(() => {
+            this.console(`${iconGrayName} was downloaded successfully!`, true);
+          });
+        } else {
+          this.console(`${iconGrayName} already exists, skip...`, true);
         }
-
-        await writeFile(this.gamePaths.appIdStatsInfoPath, resultStats.join('\r\n')).then(() => {
-          this.console(`${this.gamePaths.appIdStatsInfoPath} was written successfully!`, true);
-        });
-      } else {
-        this.console('The game has no stats.', true);
       }
+
+      await writeJson(this.gamePaths.appIdAchievementsInfoPath, resultAchievements, {
+        spaces: 2,
+      }).then(() => {
+        this.console(
+          `${this.gamePaths.appIdAchievementsInfoPath} was written successfully!`,
+          true,
+        );
+      });
     } else {
-      throw new Error(`${url}; response: ${response.statusText}`);
+      this.console('The game has no achievements.', true);
+    }
+
+    const statsData = response.data.game.availableGameStats.stats;
+    if (typeof statsData !== 'undefined' && statsData.length > 0) {
+      const resultStats = [];
+      // eslint-disable-next-line @typescript-eslint/naming-convention, canonical/id-match
+      for (const _stat of statsData) {
+        // eslint-disable-next-line @typescript-eslint/naming-convention, canonical/id-match
+        const name = _stat.name;
+        // NOTE: ..., float, avgrate but where can I find this data?
+        const typeValue = 'int';
+        // eslint-disable-next-line @typescript-eslint/naming-convention, canonical/id-match
+        const defaultValue = _stat.defaultvalue;
+
+        resultStats.push(`${name}=${typeValue}=${defaultValue}`);
+      }
+
+      await writeFile(this.gamePaths.appIdStatsInfoPath, resultStats.join('\r\n')).then(() => {
+        this.console(`${this.gamePaths.appIdStatsInfoPath} was written successfully!`, true);
+      });
+    } else {
+      this.console('The game has no stats.', true);
     }
   }
 
@@ -258,45 +268,36 @@ class SteamRetriever {
 
     let url = `https://api.steampowered.com/IInventoryService/GetItemDefMeta/v1/?key=${this.accountSteamWebApiKey}&appid=${this.gameAppId}`;
     let response = await axios.get(url);
+    const digest = response.data.response.digest;
+    // eslint-disable-next-line no-negated-condition
+    if (typeof digest !== 'undefined') {
+      url = `https://api.steampowered.com/IGameInventory/GetItemDefArchive/v1/?digest=${digest}&appid=${this.gameAppId}`;
+      response = await axios.get(url);
+      // NOTE: the last character is a finger in the ass
+      response.data = JSON.parse(response.data.slice(0, -1));
 
-    if (response.statusText === 'OK') {
-      const digest = response.data.response.digest;
-      // eslint-disable-next-line no-negated-condition
-      if (typeof digest !== 'undefined') {
-        url = `https://api.steampowered.com/IGameInventory/GetItemDefArchive/v1/?digest=${digest}&appid=${this.gameAppId}`;
-        response = await axios.get(url);
-        if (response.statusText === 'OK') {
-          // NOTE: the last character is a finger in the ass
-          response.data = JSON.parse(response.data.slice(0, -1));
+      const resultItems: Record<string, string> = {};
+      const resultDefaultItems: Record<string, number> = {};
 
-          const resultItems: Record<string, string> = {};
-          const resultDefaultItems: Record<string, number> = {};
-
-          for (const item of response.data) {
-            const itemdefid = item.itemdefid;
-            resultItems[itemdefid] = item;
-            resultDefaultItems[itemdefid] = 1;
-          }
-
-          await writeJson(this.gamePaths.appIdItemsInfoPath, resultItems, {
-            spaces: 2,
-          }).then(() => {
-            this.console(`${this.gamePaths.appIdItemsInfoPath} was written successfully!`, true);
-          });
-
-          await writeJson(this.gamePaths.appIdDefaultItemsInfoPath, resultDefaultItems, {
-            spaces: 2,
-          }).then(() => {
-            this.console(`${this.gamePaths.appIdDefaultItemsInfoPath} was written successfully!`, true);
-          });
-        } else {
-          throw new Error(`${url}; response: ${response.statusText}`);
-        }
-      } else {
-        this.console('The game has no items.', true);
+      for (const item of response.data) {
+        const itemdefid = item.itemdefid;
+        resultItems[itemdefid] = item;
+        resultDefaultItems[itemdefid] = 1;
       }
+
+      await writeJson(this.gamePaths.appIdItemsInfoPath, resultItems, {
+        spaces: 2,
+      }).then(() => {
+        this.console(`${this.gamePaths.appIdItemsInfoPath} was written successfully!`, true);
+      });
+
+      await writeJson(this.gamePaths.appIdDefaultItemsInfoPath, resultDefaultItems, {
+        spaces: 2,
+      }).then(() => {
+        this.console(`${this.gamePaths.appIdDefaultItemsInfoPath} was written successfully!`, true);
+      });
     } else {
-      throw new Error(`${url}; response: ${response.statusText}`);
+      this.console('The game has no items.', true);
     }
   }
 
@@ -306,15 +307,19 @@ class SteamRetriever {
       ...this.gameData,
     };
 
-    if (storage.has(`games.${inputs.appId}`)) {
-      notify('Game rebased successfully!');
-    } else {
-      notify('Game created successfully!');
-    }
-
     storage.set(`games.${inputs.appId}`, inputs);
 
     this.ipcEvent.send('index-reload-games-list');
+
+    if (storage.has(`games.${inputs.appId}`)) {
+      const oo = 'Game rebased successfully!';
+      notify(oo);
+      this.console(oo);
+    } else {
+      const oo = 'Game created successfully!';
+      notify(oo);
+      this.console(oo);
+    }
 
     this.consoleHide(true);
   }
@@ -322,24 +327,51 @@ class SteamRetriever {
   public async run () {
     this.consoleShow();
 
+    let stop = false;
+
     try {
       await this.getAppType();
       await this.checkSteamApiDlls();
       await this.getAppInfo();
       await this.downloadAppHeader();
 
-      if (this.accountSteamWebApiKey.length > 0) {
-        await this.getAppAchievements();
-        await this.getAppItems();
-      } else {
-        this.console('The WebApi key is missing, i skip the achievements, stats and items.');
-      }
-
-      await this.addGame();
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
-      this.console(error.message, true);
+      this.console(error, true);
       this.consoleHide();
+      stop = true;
+    }
+
+    if (!stop) {
+      try {
+        if (this.accountSteamWebApiKey.length > 0) {
+          await this.getAppAchievements();
+        } else {
+          this.console('The WebApi key is missing, i skip the achievements and stats.');
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        this.console(error, true);
+      }
+
+      try {
+        if (this.accountSteamWebApiKey.length > 0) {
+          await this.getAppItems();
+        } else {
+          this.console('The WebApi key is missing, i skip the items.');
+        }
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        this.console(error, true);
+      }
+
+      try {
+        await this.addGame();
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (error: any) {
+        this.console(error, true);
+        this.consoleHide();
+      }
     }
   }
 
